590c770a9459e2e9f9f746c5eae8336594ae1d00
Dmitry Starkov <21260939+starkdmi@users.noreply.github.com>
2023-07-06 22:05:24 +0300

    Add Image Info struct

---
+++ Sources/Types/Image/ImageInfo.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+/// Image info
+public struct ImageInfo {
+    /// Image format, default to source image format
+    let format: ImageFormat
+
+    /// Image resolution to fit in, default to source resolution
+    let size: CGSize
+
+    /// File size in kilobytes
+    // let filesize: UInt64
+
+    /// Image pixel format
+    // let pixelFormat: FourCharCode
+
+    /// Alpha channel presence
+    // let hasAlpha: Bool
+
+    /// Animation presence
+    // let isAnimated: Bool
+
+    /// Animated image frame rate
+    // let frameRate: Int?
+
+    /// Animated image duration
+    // let duration: Double
+
+    /// Metadata - date, location, device/camera/lens
+}
