c676ef7ee86cbb6d5369bfdc3a30edf57577afbd
Dmitry Starkov <21260939+starkdmi@users.noreply.github.com>
2023-07-07 13:03:24 +0300

    Update CGImage+Extensions.swift

--- Sources/Extensions/CGImage+Extensions.swift
+++ Sources/Extensions/CGImage+Extensions.swift
@@ -1,7 +1,54 @@
 import Foundation
 import CoreImage
 
-extension CGImage {
+public extension CGImage {
+    /// Alpha channel presence
+    /// Warning: Premultiplied alpha is skipped here
+    var hasAlpha: Bool {
+        return alphaInfo == CGImageAlphaInfo.first || alphaInfo == CGImageAlphaInfo.last
+    }
+
+    /// HDR data presence
+    var isHDR: Bool {
+        return bitsPerPixel >= 32 && bitsPerComponent >= 8
+    }
+
+    /// Detect Image Pixel Format based on bits information
+    func getPixelFormat() -> CIFormat {
+        switch (bitsPerPixel, bitsPerComponent) {
+        case (32, 32), (32, 64), (32, 128):
+            return .RGBAf
+        case (16, 32), (16, 64), (32, 32):
+            return .RGBAh
+        case (32, 16), (16, 16):
+            return .RGBA16
+        case (32, 8):
+            return .RGBA8
+        case (16, 8):
+            return .RG8
+        case (8, 8):
+            return .R8
+        default:
+            return .BGRA8
+        }
+    }
+
+    /// Color space
+    func getColorSpace(extendedColorSpace: Bool) -> CGColorSpace {
+        var colorSpace = self.colorSpace ?? CGColorSpaceCreateDeviceRGB()
+
+        // Replace color space with the HDR supported one when required
+        if #available(macOS 11, iOS 14, tvOS 14, *) {
+            if CGColorSpaceUsesExtendedRange(colorSpace) || CGColorSpaceUsesITUR_2100TF(colorSpace) {
+                colorSpace = CGColorSpace(name: CGColorSpace.extendedSRGB)! // CGColorSpaceCreateExtended(colorSpace)!
+            }
+        } else if CGColorSpaceUsesExtendedRange(colorSpace) {
+            colorSpace = CGColorSpace(name: CGColorSpace.extendedSRGB)! // CGColorSpaceCreateExtended(colorSpace)!
+        }
+
+        return colorSpace
+    }
+
     /// Apply multiple image operations on image
     func applyingOperations(_ operations: Set<ImageOperation>) -> CGImage {
         var image = self
@@ -13,7 +60,7 @@ extension CGImage {
             if ciContext == nil {
                 ciContext = CIContext()
             }
-            return ciImage ?? CIImage(cgImage: image)
+            return ciImage ?? CIImage(cgImage: image, options: [.applyOrientationProperty: true])
         }
         
         for operation in operations {
@@ -27,8 +74,12 @@ extension CGImage {
             }
         }
 
-        if let ciImage = ciImage, let modified = ciContext!.createCGImage(ciImage, from: ciImage.extent) {
-            image = modified
+        if let ciImage = ciImage {
+            let pixelFormat = image.getPixelFormat()
+            let colorSpace = image.getColorSpace(extendedColorSpace: !image.hasAlpha && image.isHDR)
+            if let modified = ciContext!.createCGImage(ciImage, from: ciImage.extent, format: pixelFormat, colorSpace: colorSpace) {
+                image = modified
+            }
         }
 
         return image
