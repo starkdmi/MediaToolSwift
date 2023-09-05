21371b2fcc653f213560c5152aa0b6fc43bcf3f0
Dmitry Starkov <21260939+starkdmi@users.noreply.github.com>
2023-09-05 12:45:16 +0300

    Implement image size data type

---
+++ Sources/Types/Image/ImageSize.swift
@@ -0,0 +1,30 @@
+import CoreImage
+
+/// Image frame of a static or animated image
+public enum ImageSize: Equatable {
+    /// Original size
+    case original
+
+    /// Size to fit in
+    case fit(CGSize)
+
+    /// Scale (fill) - no aspect ration preserving
+    // case scale(CGSize)
+
+    /// Cropping size and alignment, `fit` primarly used in video thumbnails
+    case crop(fit: CGSize? = nil, options: Crop)
+
+    /// Equatable conformation
+    public static func == (lhs: ImageSize, rhs: ImageSize) -> Bool {
+        switch (lhs, rhs) {
+        case (.original, .original):
+            return true
+        case (.fit(let lhsSize), .fit(let rhsSize)):
+            return lhsSize == rhsSize
+        case (let .crop(lhsSize, lhsOptions), let .crop(rhsSize, rhsOptions)):
+            return lhsSize == rhsSize && lhsOptions == rhsOptions
+        default:
+            return false
+        }
+    }
+}
