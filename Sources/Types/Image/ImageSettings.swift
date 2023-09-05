f9eb4d51b7f7bbd37226831f6292fe30c665c20c
Dmitry Starkov <21260939+starkdmi@users.noreply.github.com>
2023-09-05 12:49:54 +0300

    Update image settings

--- Sources/Types/Image/ImageSettings.swift
+++ Sources/Types/Image/ImageSettings.swift
@@ -6,51 +6,63 @@ public struct ImageSettings {
     /// Public initializer
     public init(
         format: ImageFormat? = nil,
-        size: CGSize? = nil, quality: Double? = nil,
+        size: ImageSize = .original,
+        quality: Double? = nil,
         frameRate: Int? = nil,
+        skipAnimation: Bool = false,
         preserveAlphaChannel: Bool = true,
         embedThumbnail: Bool = false,
         optimizeColorForSharing: Bool = false,
         backgroundColor: CGColor? = nil,
-        edit: Set<ImageOperation> = []
+        edit: Set<ImageOperation> = [],
+        preferredFramework: ImageFramework = .ciImage
     ) {
         self.format = format
         self.size = size
         self.quality = quality
         self.frameRate = frameRate
+        self.skipAnimation = skipAnimation
         self.preserveAlphaChannel = preserveAlphaChannel
         self.embedThumbnail = embedThumbnail
         self.optimizeColorForSharing = optimizeColorForSharing
         self.backgroundColor = backgroundColor
         self.edit = edit
+        self.preferredFramework = preferredFramework
     }
 
     /// Image format, default to source image format
     var format: ImageFormat?
 
-    /// Image resolution to fit in, default to source resolution
-    let size: CGSize?
+    /// Image size and crop options, default to source resolution
+    var size: ImageSize
 
     /// Image quality, from 0.0 to 1.0 where 1.0 is lossless, not all `ImageFormat` supported
-    let quality: Double?
+    var quality: Double?
 
     /// Animated image frame rate
-    let frameRate: Int?
+    var frameRate: Int?
+
+    /// Preserve only main (HEIF) or first frame of animated image
+    var skipAnimation: Bool
 
     /// Indicates to save alpha channel or drop
     /// Do nothing for images files without alpha channel
-    let preserveAlphaChannel: Bool
+    var preserveAlphaChannel: Bool
 
     /// Embed light version of image to file, JPEG and HEIF only, default to false
-    let embedThumbnail: Bool
+    var embedThumbnail: Bool
 
     /// Modify image color space to support older devices, default to false
-    let optimizeColorForSharing: Bool
+    var optimizeColorForSharing: Bool
 
     /// Background color for image formats without transparency support, default to white
-    let backgroundColor: CGColor?
+    var backgroundColor: CGColor?
 
     /// Image specific operations like crop, filter, atd.
     /// Only one operation of each type is applied
-    let edit: Set<ImageOperation>
+    var edit: Set<ImageOperation>
+
+    /// Image framework used for loading, editing and saving
+    /// Not always taken into account
+    var preferredFramework: ImageFramework
 }
