7f3002091a700cd2743af5f475b3fb13480860aa
Dmitry Starkov <21260939+starkdmi@users.noreply.github.com>
2023-10-23 21:30:13 +0300

    Fix struct properties marked as internal

--- Sources/Types/Image/ImageInfo.swift
+++ Sources/Types/Image/ImageInfo.swift
@@ -2,41 +2,62 @@ import CoreImage
 
 /// Image info
 public struct ImageInfo {
+    /// Public initializer
+    /*public init(
+        format: ImageFormat,
+        size: CGSize,
+        hasAlpha: Bool,
+        isHDR: Bool,
+        orientation: CGImagePropertyOrientation? = nil,
+        framesCount: Int,
+        frameRate: Int? = nil,
+        duration: Double? = nil
+    ) {
+        self.format = format
+        self.size = size
+        self.hasAlpha = hasAlpha
+        self.isHDR = isHDR
+        self.orientation = orientation
+        self.framesCount = framesCount
+        self.frameRate = frameRate
+        self.duration = duration
+    }*/
+
     /// Image format, default to source image format
-    let format: ImageFormat
+    public let format: ImageFormat
 
     /// Image resolution to fit in, default to source resolution
-    let size: CGSize
+    public let size: CGSize
 
     /// File size in kilobytes
-    // let filesize: UInt64
+    // public let filesize: UInt64
 
     /// Image pixel format
-    // let pixelFormat: FourCharCode
+    // public let pixelFormat: FourCharCode
 
     /// Alpha channel presence
-    let hasAlpha: Bool
+    public let hasAlpha: Bool
 
     /// HDR data presence
-    let isHDR: Bool
+    public let isHDR: Bool
 
     /// Bit depth | bits per component
-    // let bitDepth: Int
+    // public let bitDepth: Int
 
     /// Image orientation
-    var orientation: CGImagePropertyOrientation?
+    public var orientation: CGImagePropertyOrientation?
 
     /// Image frames amount, one for static images
-    let framesCount: Int
+    public let framesCount: Int
 
     /// Animated image frame rate
-    let frameRate: Int?
+    public let frameRate: Int?
 
     /// Animated image duration
-    let duration: Double?
+    public let duration: Double?
 
     /// Animation presence
-    var isAnimated: Bool {
+    public var isAnimated: Bool {
         return framesCount > 1
     }
 }
